5004aa4ed04866831e58ebb8b4b1703918148938
Lluc <99950000+Llucaieaie@users.noreply.github.com>
2022-11-12 18:31:31 +0100

    items

--- GameDevelopment/citm_desvj_project_template-L08/Game/Source/Item.cpp
+++ GameDevelopment/citm_desvj_project_template-L08/Game/Source/Item.cpp
@@ -42,7 +42,7 @@ bool Item::Start() {
 	currentAnimation = &money;
 	
 	// L07 DONE 4: Add a physics to an item - initialize the physics body
-	pbody = app->physics->CreateCircle(position.x, position.y, 12, bodyType::DYNAMIC);
+	pbody = app->physics->CreateCircle(position.x, position.y, 12, bodyType::STATIC);
 
 	// L07 DONE 7: Assign collider type
 	pbody->ctype = ColliderType::ITEM;
